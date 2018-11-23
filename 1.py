0fcdb76670d77985ac814d6328d56dcb8a0ca081
kkdave28 <kkdave@uci.edu>
2018-11-23 09:32:47 -0800

    removed python files

--- 1.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env/python3
-import math
-def factorize(n):
-    divisors = [ d for d in range(2,int(math.sqrt(n))) if n % d == 0 ]
-    print(divisors)
-
-#def main():
-factorize(337123)
